3a135ff6a990ab46db34da75bf5da1e8dd3ac9c3
Shweta Shetty <shettyshweta48@gmail.com>
2024-11-15 23:59:44 +0530

    Add files via upload

---
+++ Practical Questions/Question1/1st.py
@@ -0,0 +1,5 @@
+#WAP to find the greatest amount three numbers.
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+print("Greatest number is:", max(a, b, c))
